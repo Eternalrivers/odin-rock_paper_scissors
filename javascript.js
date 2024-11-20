3bb9e67ff10335ccf8b24cab5433957fec48a1c6
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2024-11-21 06:43:24 +0800

    feat: Create function that generates random number

--- javascript.js
+++ javascript.js
@@ -1,2 +1,7 @@
 console.log("Hello World!")
 console.log("John")
+
+function getRandom() {
+  return  Math.floor((Math.random()) * (100-1) + 1);
+}
+//  A function that returns a random number between 100 and 0.
