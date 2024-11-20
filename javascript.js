34b90b404605ad7fe0e93739d0f0c9ecf7794c14
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2024-11-21 06:44:40 +0800

    feat (!): Add function for "getComputerChoice"

--- javascript.js
+++ javascript.js
@@ -5,3 +5,21 @@ function getRandom() {
   return  Math.floor((Math.random()) * (100-1) + 1);
 }
 //  A function that returns a random number between 100 and 0.
+function getComputerChoice(){
+
+  let choice = getRandom ();
+
+  console.log (choice);
+  
+  if (choice < 34 && choice > 0) {
+   return ("rock");
+  
+  } else if (choice < 67 && choice > 33) {
+    return ("paper");
+  
+  }else  {
+    return ("scissors");
+  }
+}
+// Random number generated is assigned a string value.
+
