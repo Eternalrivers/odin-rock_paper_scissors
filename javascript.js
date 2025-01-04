92c550e0d81a3fbfe93bf0cd564f7fbee6beabd4
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-01-05 01:18:50 +0800

    refactor: rework to display result and score.
    Rewrite code to change the method to display score as previous method
    only shows on the console. As scores was not updating each time a side
    scores. This was fixed by post incrementing the score.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function getRandom() {
   return  Math.floor((Math.random()) * 3);
 }
-//  A function that returns a random number between 100 and 0.
+//  A function that returns a random number between 2 and 0.
 function getComputerChoice(){
   const choice = getRandom ();
   
@@ -26,37 +26,42 @@ function getHumanChoice() {
 let initHumanScore = 0;
 let initComputerScore = 0;
 
+
 //uses string values from previous functions
 function playRound(humanChoice) {
   const computerChoice = getComputerChoice ();
-    
-  if (humanChoice === computerChoice) {
-      console.log ("draw");
   
+  if (humanChoice === computerChoice) {
+    messageResult.textContent = ('draw');
+        
   } else if (humanChoice === "rock" && computerChoice === "paper") {
-      console.log ("You lose! Paper beats rock.");
-      return (++initComputerScore);
+    initComputerScore++;
+    messageResult.textContent = ("You lose! Paper beats rock.");
   
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-      console.log ("You won! Rock beats scissors.");
-      return (++initHumanScore);
+    initHumanScore++;
+    messageResult.textContent = ("You won! Rock beats scissors.");
   }
    else if (humanChoice === "paper" && computerChoice === "scissors") {
-      console.log ("You lose! Scissors beats paper.");
-      return (++initComputerScore);
+      initComputerScore++;
+      messageResult.textContent = ("You lose! Scissors beats paper.");
 
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-      console.log ("You won! Paper beats rock.");
-      return (++initHumanScore);
-  
+      initHumanScore++;
+      messageResult.textContent = ("You won! Paper beats rock.");
+       
   }else if (humanChoice === "scissors" && computerChoice === "paper") {
-      console.log ("You won! Scissors beats paper.");
-      return (++initHumanScore);
+      initHumanScore++;
+      messageResult.textContent = ("You won! Scissors beats paper.");
   
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
-      console.log ("You lose! Rock beats scissors.");
-      return (++initComputerScore);
+      initComputerScore++;
+      messageResult.textContent = ("You lose! Rock beats scissors.");
   }
+ 
+  hum.textContent =(`Human score ${initHumanScore}`);
+  comp.textContent =(`Computer score ${initComputerScore}`);
+
 }
 
 function finalScore() {
@@ -74,6 +79,7 @@ function finalScore() {
 const btnRock = document.querySelector('#rock');
 const btnPaper = document.querySelector('#paper');
 const btnScissors = document.querySelector('#scissors');
+
 
 /**
  * The computerChoice variable has its variable in a constant value. 
@@ -95,3 +101,10 @@ btnScissors.addEventListener ('click', function(e) {
  * The computerChoice variable has its variable in a constant value. Meaning that any presses of the button will result in the computer having the same choice.
  * The variable computerChoice was removed moving the variable to the function itself.
  */
+const hum = document.querySelector('#humanScore');
+const comp = document.querySelector('#computerScore');
+const messageResult = document.querySelector('.message');
+
+
+hum.textContent =(`Human score ${initHumanScore}`);
+comp.textContent =(`Computer score ${initComputerScore}`);
