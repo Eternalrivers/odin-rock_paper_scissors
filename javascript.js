64447f895580f29bdf03727b517d8f3374eece9d
Eternalrivers <158377388+Eternalrivers@users.noreply.github.com>
2025-01-06 12:17:53 +0800

    feat: add button to reset the game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 function getRandom() {
   return  Math.floor((Math.random()) * 3);
 }
+
 //  A function that returns a random number between 2 and 0.
 function getComputerChoice(){
   const choice = getRandom ();
@@ -16,9 +17,7 @@ function getComputerChoice(){
   }
 }
 
-
 // Random number generated is assigned a string value.
-
 let initHumanScore = 0;
 let initComputerScore = 0;
 
@@ -72,8 +71,11 @@ const btn = document.querySelectorAll ('button');
 const hum = document.querySelector('#humanScore');
 const comp = document.querySelector('#computerScore');
 const messageResult = document.querySelector('.message');
-const btns = document.querySelector("button");
-const div = document.querySelector('div');
+const btns = document.querySelectorAll("button");
+const btnGrp = document.querySelector('.button');
+
+hum.textContent =(`Human score ${initHumanScore}`);
+comp.textContent =(`Computer score ${initComputerScore}`);
 
 
 /**
@@ -82,60 +84,52 @@ const div = document.querySelector('div');
  * The variable computerChoice was removed as a parameter for playRound moving the variable to the function itself.
  * 1st param for the playRound comes from the button using the dataset.
  */
-btnRock.addEventListener ('click', function(e) {
-  if (initComputerScore === 5 || initHumanScore === 5) {
-    finalScore ();
-    removeButtons();
-  } 
-  playRound (e.target.dataset.value);
-
-  hum.textContent =(`Human score ${initHumanScore}`);
-  comp.textContent =(`Computer score ${initComputerScore}`);
-
-  if (initComputerScore === 5 || initHumanScore === 5) {
-    finalScore ();  
-}
-});
-
-btnPaper.addEventListener ('click', function(e) {
-  if (initComputerScore === 5 || initHumanScore === 5) {
-    finalScore ();
-    removeButtons();
+btns.forEach(btn => {
+  btn.addEventListener ('click', function(e) {
+    playRound (e.target.dataset.value);
   
-  } 
-  playRound (e.target.dataset.value);
-  hum.textContent =(`Human score ${initHumanScore}`);
-  comp.textContent =(`Computer score ${initComputerScore}`);
-
-  if (initComputerScore === 5 || initHumanScore === 5) {
-    finalScore ();
-}
-});
-btnScissors.addEventListener ('click', function(e) {
-  if (initComputerScore === 5 || initHumanScore === 5) {
-    finalScore ();
-    removeButtons();
+    hum.textContent =(`Human score ${initHumanScore}`);
+    comp.textContent =(`Computer score ${initComputerScore}`);
   
-  } 
-  playRound (e.target.dataset.value);
-  hum.textContent =(`Human score ${initHumanScore}`);
-  comp.textContent =(`Computer score ${initComputerScore}`);
-
-  if (initComputerScore === 5 || initHumanScore === 5) {
-    finalScore ();
-}
+    if (initComputerScore === 5 || initHumanScore === 5) {
+      finalScore ();
+      removeButtons();  
+      resetBtn ();
+    } 
+  });
 });
 
-/**
- * The computerChoice variable has its variable in a constant value. Meaning that any presses of the button will result in the computer having the same choice.
- * The variable computerChoice was removed moving the variable to the function itself.
- */
+function removeButtons() {
+btnGrp.removeChild(btnRock);
+btnGrp.removeChild(btnPaper);
+btnGrp.removeChild(btnScissors);
+}
 
+function resetBtn () {
+  const newBtn = document.createElement ('button');
+  newBtn.textContent = ('Reset');
+  
+  btnGrp.appendChild(newBtn);
+  newBtn.classList.add('reset');
+  newBtn.addEventListener('click', reset);
+}
+
+
+function reset() {
+initComputerScore = 0;
+initHumanScore = 0;
 
 hum.textContent =(`Human score ${initHumanScore}`);
 comp.textContent =(`Computer score ${initComputerScore}`);
+messageResult.textContent = '';
+
+const resBtn = document.querySelector('.reset');
+
+btnGrp.removeChild(resBtn);
+
+btnGrp.appendChild(btnRock);
+btnGrp.appendChild(btnPaper);
+btnGrp.appendChild(btnScissors);
 
 
-function removeButtons() {
-  div.removeChild("btns");
 }
